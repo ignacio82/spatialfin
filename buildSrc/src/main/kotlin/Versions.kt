d0edab586885c3b78c5ae446a7ad678e3bc4224c
Ignacio <ignacio@example.com>
2026-03-15 18:14:27 +0000

    chore: bump version to 1.0.5

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object Versions {
-    const val APP_CODE = 34
-    const val APP_NAME = "1.0.4"
+    const val APP_CODE = 35
+    const val APP_NAME = "1.0.5"
 
     const val COMPILE_SDK = 36
     const val TARGET_SDK = 36
