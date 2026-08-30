4a5aa66b11c8de99729b816cbe2293723c659317
ignacio <ignacio@martinez.fyi>
2026-08-30 23:23:59 +0000

    build: bump to 2.7.57 (158)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object Versions {
-    const val APP_CODE = 157
-    const val APP_NAME = "2.7.56"
+    const val APP_CODE = 158
+    const val APP_NAME = "2.7.57"
 
     const val COMPILE_SDK = 37
     const val TARGET_SDK = 36
